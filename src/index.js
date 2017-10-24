93585596a995ecc943f31329d2fd15795f4233e0
Andrey Shuvalov <6286552+dronbas@users.noreply.github.com>
2017-10-25 01:23:05 +0300

    add sources

---
+++ src/index.js
@@ -0,0 +1,79 @@
+/**
+ * @typedef {object} Condition
+ * @property {(number|string)} id
+ * @property {function} check - validate function.
+ *
+ * @param {Condition} cond
+ * @throws Error if there is no id provided
+ */
+const checkCfg = cond => {
+  if (cond instanceof Object !== true) {
+    throw new Error('expect condition to be an object')
+  }
+
+  if (cond.id === undefined) {
+    throw new Error('expect id to be provided')
+  }
+
+  if (typeof cond.id !== 'number' && typeof cond.id !== 'string') {
+    throw new Error(`expect id to be a number or string, id is ${typeof cond.id}`)
+  }
+
+  if (typeof cond.check !== 'function') {
+    throw new Error('expect check to be a function')
+  }
+};
+
+module.exports = class Validation {
+  constructor() {
+    this.condList = {};
+  }
+
+  /**
+   *
+   * @param {Condition} condition
+   */
+  add(condition) {
+    checkCfg(condition);
+
+    if (this.condList[condition.id] === undefined) {
+      this.condList[condition.id] = condition;
+    }
+  }
+
+  /**
+   *
+   * @param {array<Condition>} conditions
+   */
+  addMulti(conditions) {
+    if (Array.isArray(conditions) === false) {
+      throw new Error('expect conditions to be an array')
+    }
+
+    conditions.forEach(cond => this.add(cond))
+  }
+
+  /**
+   *
+   * @typedef {object} ConditionConfig
+   * @property {(number|string)} id
+   * @property {object} params - additional condition params.
+   *
+   * @param {array.<ConditionConfig>} configs
+   * @param {object} values
+   * @returns {boolean}
+   */
+  exec(configs, values) {
+    if (Array.isArray(configs) === false) {
+      throw new Error('expect configs to be an array');
+    }
+    return configs.every(cfg => {
+      const cond = this.condList[cfg.id];
+      if (cond === undefined) {
+        throw new Error(`expect condition module #${cfg.id} to be added`)
+      }
+
+      return cond.check(values, cfg.params);
+    })
+  }
+};
---
+++ test/init.js
@@ -0,0 +1,60 @@
+const expect = require('chai').expect;
+const Validation = require('../src');
+
+describe('initialization', () => {
+  const id = 777;
+  const check = () => true;
+  let validator;
+
+  beforeEach(() => {
+    validator = new Validation();
+  });
+
+  it('should be initialized with condList object', () => {
+    expect(validator).to.deep.eql({condList: {}});
+  });
+
+  it('add - should throw an error if there is no condition provided', () => {
+    expect(() => validator.add()).to.throw('expect condition to be an object');
+  });
+
+  it('add - should throw an error if there is no id provided', () => {
+    const badCondition = {};
+    expect(() => validator.add(badCondition)).to.throw('expect id to be provided');
+  });
+
+  it('add - should throw an error if id is null', () => {
+    const badCondition = {id: null};
+    expect(() => validator.add(badCondition)).to.throw(/expect id to be a number or string/);
+  });
+
+  it('add - should throw an error if id type is bool', () => {
+    const badCondition = {id: true};
+    expect(() => validator.add(badCondition)).to.throw(/expect id to be a number or string/);
+  });
+
+  it('add - should throw an error if id type is object', () => {
+    const badCondition = {id: {}};
+    expect(() => validator.add(badCondition)).to.throw(/expect id to be a number or string/);
+  });
+
+  it('add - should throw an error if check type is not function', () => {
+    const badCondition = {id, check: {}};
+    expect(() => validator.add(badCondition)).to.throw('expect check to be a function');
+  });
+
+  it('add - should add new condition to list', () => {
+    const condition = {id, check};
+    validator.add(condition)
+    expect(validator).to.be.deep.eql({condList: {777: condition}})
+  });
+
+  it('addMulti - should throw an error if argument is not an array', () => {
+    expect(() => validator.addMulti({id, check})).to.throw('expect conditions to be an array')
+  });
+
+  it('should add multiple conditions to list', () => {
+    validator.addMulti([{id: 1, check}, {id: '2', check}]);
+    expect(validator).to.be.deep.eql({condList: {1: {id: 1, check}, '2': {id: '2', check}}})
+  });
+});
